e6016937a5faedbcd80f382790a0b3079a049aa1
zsytssk <zsytssk@gmail.com>
2022-01-10 19:50:17 +0800

    master:

--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,10 @@
 import { init } from 'lightCanvas/main';
 import { drawImage } from './image';
-import { testPivot } from './matrix/pivot';
+import { testPivot, testPivot2 } from './matrix/pivot';
 import { testScale } from './node/scale';
 
 function main() {
     const { stage } = init(750, 1334, { bg_color: '#000' });
-    drawImage(stage);
+    testPivot2(stage);
 }
 main();
