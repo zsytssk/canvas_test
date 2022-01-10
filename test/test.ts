0890e0f530c73638e1f7132c01756a75a40a7a11
zsytssk <zsytssk@gmail.com>
2022-01-10 19:48:16 +0800

    master:

--- test/test.ts
+++ test/test.ts
@@ -4,7 +4,7 @@ import { testPivot } from './matrix/pivot';
 import { testScale } from './node/scale';
 
 function main() {
-    const { stage } = init(1334, 750, { bg_color: '#000' });
+    const { stage } = init(750, 1334, { bg_color: '#000' });
     drawImage(stage);
 }
 main();
